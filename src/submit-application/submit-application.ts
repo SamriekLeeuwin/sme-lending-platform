530f4529bc89bdcf2a99d23c5d9728e054d0e617
SamriekLeeuwin <semriekk@gmail.com>
2026-07-07 22:25:59 +0200

    InterFace Data

---
+++ src/submit-application/submit-application.ts
@@ -0,0 +1,15 @@
+interface SubmitApplicationData{
+applicationId: number;
+createdAt: string;
+applicantName: string;
+companyName: string;
+email: string;
+existingDebt: number;
+loanAmount: number;
+loanPurpose: string;
+loanTermMonths: number;
+monthlyCashflow: number;
+phoneNumber: string;
+status :string;
+}
+
